738ce33e64e6d69bb56c34f393179a837cf6bcfe
xiaobinliuu <95059359+xiaobinliuu@users.noreply.github.com>
2021-12-10 21:42:49 +0800

    Update word_occurrences.py

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,7 +1,7 @@
 def main():
-    user_string = input("Text:")
+    user_string = input("Text:")   
     words = user_string.split()
-    Word_To_Number = {}
+    Word_To_Number = {}     # nice naming
 
     for word in words:
         Word_To_Number[word] = Word_To_Number.get(word, 0) + 1
@@ -9,6 +9,7 @@ def main():
     for word, value in Word_To_Number.items():
         print("{:6} : {:5}".format(word, value))
 
+        # code all good, but the output is not sorted with order. 
 
 if __name__ == '__main__':
     main()
